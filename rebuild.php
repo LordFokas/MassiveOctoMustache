c0acfe5500acfcaef141a3a6912d04fc909cb2c8
Josh <JoshTheEnder@users.noreply.github.com>
2014-05-20 13:07:22 +0100

    Update rebuild.php

--- rebuild.php
+++ rebuild.php
@@ -2,7 +2,7 @@
 	# The next time someone attempts to edit
 	# this file I will personally string him
 	# up from his own fucking intestines!
-	
+	# olololol
 	ini_set("log_errors", 1);
 	ini_set("error_log", "./php-error.log");
 	
