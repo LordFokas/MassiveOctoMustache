9720222b226dde206946f5319f2062d9d6180d9d
Lizzy Trickster <that.joshua.green@gmail.com>
2015-12-05 18:02:37 +0000

    link changes "../" links changed to just "/" because they're html and relative to the site root Changed "http://" to just "//" as this allows for external linking but links relative to what scheme (read: http. https) the page was requested as

--- _builder/rendering/item.php
+++ _builder/rendering/item.php
@@ -6,7 +6,7 @@
 		?>
 		<div class="ui segment">
 			<div class="column128">
-				<img src="../_resources/img/item/<?php echo $item->getIcon(); ?>" class="icon128" />
+				<img src="/_resources/img/item/<?php echo $item->getIcon(); ?>" class="icon128" />
 			</div>
 			<div class="column730">
 				<h2 style="margin-top: 0px;"><?php echo $item->getName(); ?></h2>
